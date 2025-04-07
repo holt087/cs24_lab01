8bd926acf0f5cce3bd84d14ec96c66c7119f04e3
Grace Holtzclaw <holtzclaw@ucsb.edu>
2025-04-06 23:12:25 -0700

    3sum analysis changes

--- 3sum_analysis.cpp
+++ 3sum_analysis.cpp
@@ -3,34 +3,32 @@
 #include <chrono>
 using namespace std;
 
-
 vector<vector<int>> threeSum(vector<int>& nums) {
     vector<vector<int>> vect;
     for (int i = 0; i <nums.size(); i++) {
         for (int j = i+1; j <nums.size(); j++) {
             for (int k = j+1; k <nums.size(); k++) {
-                if (nums.at(i) + nums.at(j) + nums.at(k) == 0) {
+                if (nums.at(i) + nums.at(j) + nums.at(k) == 0) { //sorts vector, least to greatest
                     int min = nums.at(i);
                     if (nums.at(j) <= nums.at(i) && nums.at(j) <= nums.at(k)) min = nums.at(j);
-                        if (nums.at(k) <= nums.at(i) && nums.at(k) <= nums.at(j)) min = nums.at(k);
-                        int max = nums.at(i);
-                        if (nums.at(j) >= nums.at(i) && nums.at(j) >= nums.at(k)) max = nums.at(j);
-                        if (nums.at(k) >= nums.at(i) && nums.at(k) >= nums.at(j)) max = nums.at(k);
-                        int mid =  nums.at(i) + nums.at(j) + nums.at(k) - max - min;
+                    if (nums.at(k) <= nums.at(i) && nums.at(k) <= nums.at(j)) min = nums.at(k);
+                    int max = nums.at(i);
+                    if (nums.at(j) >= nums.at(i) && nums.at(j) >= nums.at(k)) max = nums.at(j);
+                    if (nums.at(k) >= nums.at(i) && nums.at(k) >= nums.at(j)) max = nums.at(k);
+                    int mid =  nums.at(i) + nums.at(j) + nums.at(k) - max - min;
                         
-                        vector<int> triple = {min, mid, max};
-                        bool duplicate = false;
-                        for (vector<int> threeSet : vect) {
+                    vector<int> triple = {min, mid, max};  //checks if three set already in vect
+                    bool duplicate = false;
+                    for (vector<int> threeSet : vect) {
                         if (triple == threeSet) duplicate = true;
-                        }
-                        
-                        if (duplicate == false) vect.push_back(triple);
                     }
+                    if (duplicate == false) vect.push_back(triple); //if not in vect already, adds it to vect
                 }
             }
         }
-        return vect;
     }
+    return vect;
+}
 
 vector<int> createSet(int numOfElem) {
     return {}; //FIXME, iterative to create set with a least one soultion, (-a, -b, a+b), hardcode???
