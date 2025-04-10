5126b1e66c733b74fa8947bc8c491ac3ab729db9
Grace Holtzclaw <holtzclaw@ucsb.edu>
2025-04-10 10:56:39 -0700

    final changes to 3sum

--- 3sum_analysis.cpp
+++ 3sum_analysis.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <set>
 #include <chrono>
 #include <cstdlib>
 using namespace std;
 
 vector<vector<int>> threeSum(vector<int>& nums) {
+    set<vector<int>> unique;
     vector<vector<int>> vect;
+
     for (int i = 0; i <nums.size(); i++) {
         for (int j = i+1; j <nums.size(); j++) {
             for (int k = j+1; k <nums.size(); k++) {
-                if (nums.at(i) + nums.at(j) + nums.at(k) == 0) { //sorts vector, least to greatest
+                if (nums.at(i) + nums.at(j) + nums.at(k) == 0) {
                     int min = nums.at(i);
                     if (nums.at(j) <= nums.at(i) && nums.at(j) <= nums.at(k)) min = nums.at(j);
                     if (nums.at(k) <= nums.at(i) && nums.at(k) <= nums.at(j)) min = nums.at(k);
@@ -17,17 +20,17 @@ vector<vector<int>> threeSum(vector<int>& nums) {
                     if (nums.at(j) >= nums.at(i) && nums.at(j) >= nums.at(k)) max = nums.at(j);
                     if (nums.at(k) >= nums.at(i) && nums.at(k) >= nums.at(j)) max = nums.at(k);
                     int mid =  nums.at(i) + nums.at(j) + nums.at(k) - max - min;
-                        
-                    vector<int> triple = {min, mid, max};  //checks if three set already in vect
-                    bool duplicate = false;
-                    for (vector<int> threeSet : vect) {
-                        if (triple == threeSet) duplicate = true;
-                    }
-                    if (duplicate == false) vect.push_back(triple); //if not in vect already, adds it to vect
+                    
+                    vector<int> triple = {min, mid, max};
+                    unique.insert(triple);
                 }
             }
         }
     }
+    for (const auto& triplet : unique) {
+    vect.push_back(triplet);
+    }
+
     return vect;
 }
 
@@ -36,7 +39,7 @@ vector<int> createSet(int numOfElem) {
     for (int i = 0; i < numOfElem; i++) {
         randomSet.push_back(rand() % 21 - 10);
     }
-    return randomSet; //FIXME, at least one soultion of (-a, -b, a+b), hardcode???
+    return randomSet;
 }
 
 double timing(vector<int>& nums) {
@@ -55,9 +58,18 @@ int main() {
     set = createSet(400);
     cout << "n = 400  |  time = " << timing(set) << endl;
     set = createSet(800);
-    cout << "n = 800 |  time = " << timing(set) << endl;
+    cout << "n = 800  |  time = " << timing(set) << endl;
     set = createSet(1600);
     cout << "n = 1600 |  time = " << timing(set) << endl;
+    
+    // TEST purposes
+    // auto result = threeSum(set);
+    // for (auto triple : result) {
+    //     for (auto one : triple) {
+    //         cout << one << ", ";
+    //     }
+    //     cout << endl;
+    // }
 
     return 0;
 }
