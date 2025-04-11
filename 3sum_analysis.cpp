0184750213c1310e1fb79e4dbb20d943f7f5604f
Grace Holtzclaw <holtzclaw@ucsb.edu>
2025-04-10 17:44:22 -0700

    final changes

--- 3sum_analysis.cpp
+++ 3sum_analysis.cpp
@@ -22,10 +22,34 @@ vector<vector<int>> threeSum(vector<int>& nums) {
         }
     }
     for (const auto& triplet : unique) {
-    vect.push_back(triplet);
+        vect.push_back(triplet);
     }
 
     return vect;
+}
+
+vector<vector<int>> betterThreeSum(vector<int>& nums) {   
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> resultVect;
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (i > 0 && nums.at(i) == nums.at(i-1)) continue;
+        int beg = i+1;
+        int end = nums.size()-1;
+        while (beg < end) {
+            int sum = nums.at(i) + nums.at(beg) + nums.at(end);
+            if (sum == 0) {
+                resultVect.push_back({nums.at(i), nums.at(beg), nums.at(end)});
+                beg++;
+                end--;
+                while (beg < end && nums[beg] == nums[beg - 1]) beg++;
+                while (beg < end && nums[end] == nums[end + 1]) end--;
+            }
+            else if (sum < 0) beg++;
+            else if (sum > 0) end--;
+        }
+    }
+    return resultVect;
 }
 
 vector<int> createSet(int numOfElem) {
@@ -36,9 +60,11 @@ vector<int> createSet(int numOfElem) {
     return randomSet;
 }
 
-double timing(vector<int>& nums) {
+double timing(vector<int>& nums, char c) {
     auto start = chrono::high_resolution_clock::now();
-    vector<vector<int>> result = threeSum(nums);
+    vector<vector<int>> result;
+    if (c == 'b') result = betterThreeSum(nums);
+    else result = threeSum(nums);
     auto end = chrono::high_resolution_clock::now();
     double time_ms = chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0;
     return time_ms;
@@ -48,17 +74,23 @@ int main() {
     vector<int> set;
 
     set = createSet(200);
-    cout << "n = 200  |  time = " << timing(set) << endl;
+    cout << "n = 200  |  time = " << timing(set, 'a') << endl;
+    cout << "n = 200  |  time = " << timing(set, 'b') << endl;
     set = createSet(400);
-    cout << "n = 400  |  time = " << timing(set) << endl;
+    cout << "n = 400  |  time = " << timing(set, 'a') << endl;
+    cout << "n = 400  |  time = " << timing(set, 'b') << endl;
     set = createSet(800);
-    cout << "n = 800  |  time = " << timing(set) << endl;
+    cout << "n = 800  |  time = " << timing(set, 'a') << endl;
+    cout << "n = 800  |  time = " << timing(set, 'b') << endl;
     set = createSet(1600);
-    cout << "n = 1600 |  time = " << timing(set) << endl;
+    cout << "n = 1600 |  time = " << timing(set, 'a') << endl;
+    cout << "n = 1600 |  time = " << timing(set, 'b') << endl;
     set = createSet(2400);
-    cout << "n = 2400 |  time = " << timing(set) << endl;
+    cout << "n = 2400 |  time = " << timing(set, 'a') << endl;
+    cout << "n = 2400 |  time = " << timing(set, 'b') << endl;
     set = createSet(3200);
-    cout << "n = 3200 |  time = " << timing(set) << endl;
+    cout << "n = 3200 |  time = " << timing(set, 'a') << endl;
+    cout << "n = 3200 |  time = " << timing(set, 'b') << endl;
 
     // TEST purposes
     // auto result = threeSum(set);
